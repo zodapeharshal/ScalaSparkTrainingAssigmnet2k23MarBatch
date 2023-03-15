dad3b57d937d4cb4173e65797f28bad3cdf0ac6f
mehul rastogi <rastogimehul01@gmail.com>
2023-03-15 23:04:59 +0530

    added recursive code to fins last index of list and assertion for the same question is added

--- src/main/scala/Mehul/Main.scala
+++ src/main/scala/Mehul/Main.scala
@@ -1,5 +1,8 @@
 package Mehul
+//import Mehul.Lastindex.List_last_index.add
+import Mehul.List_last.list_recursion
+object Main {
+  //println("Welcome to Scala")
+  assert(list_recursion(List(2,3,4,5))==5,"wrong answer")
 
-object Main extends App {
-  println("Welcome to Scala")
 }
