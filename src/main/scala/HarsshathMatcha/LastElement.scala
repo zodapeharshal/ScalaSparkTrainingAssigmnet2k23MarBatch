aa7d03cf017ceccbc7466ec5cc57febf37874a4a
Harsshath23 <harsshath0819@gmail.com>
2023-03-15 23:19:35 +0530

    Last element of the list

--- src/main/scala/HarsshathMatcha/LastElement.scala
+++ src/main/scala/HarsshathMatcha/LastElement.scala
@@ -3,14 +3,15 @@ import scala.annotation._
 object LastElement {
   def last(list:List[Int]):Int={
     @tailrec
-    def lastElement(list:List[Int],i:Int):Int={
-     if(i==list.length-1) list(i)
-     else lastElement(list,i+1)
+        def lastEle(list: List[Int], lastElement: Int): Int = {
+          if (list.isEmpty) lastElement
+          else lastEle(list.tail, list.head)
+        }
+        lastEle(list, 0)
+      }
+    //val list = List(1,2,3,4,5,6)
+     //println(last(list))
+
     }
-    lastElement(list,0)
 
-  }
-  val list=List(1,2,3,4,5,6)
-  //println(last(list))
 
-}
