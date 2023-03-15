3423e72b39106a04ea315a5cb170d90ae214a5ff
pranav <sripranav.y19@iiits.in>
2023-03-15 21:11:27 +0530

    last element of list

--- src/main/scala/YintiSriPranav/Main.scala
+++ src/main/scala/YintiSriPranav/Main.scala
@@ -1,8 +1,11 @@
 package YintiSriPranav
 import YintiSriPranav.factorial.factorial
 import YintiSriPranav.Fibonacci.fibonacci
+import YintiSriPranav.lastelement.last
+
 object Main extends App {
   //println("Welcome to Scala")
   assert(factorial(5,1)==120,"wrong result")
   assert(fibonacci (10)==34, "wrong result")
+  assert(last(List(1,2,3,4,5,6))==6)
 }
