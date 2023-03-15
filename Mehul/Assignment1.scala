d0863d305137bf54242624de6207cc22ba2b93c7
mehul rastogi <rastogimehul01@gmail.com>
2023-03-15 13:22:03 +0530

    assignment1 added using functions

---
+++ Mehul/Assignment1.scala
@@ -0,0 +1,9 @@
+object Assignment1 {
+  def main(args:Array[String]){
+
+    val array=Array[Int](1,4 ,7,9,16,25,36,49)
+    val count = array.count(x => Math.sqrt(x)%1==0)
+    print(count)
+  }
+}
+
