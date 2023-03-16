cb6c563960cc2c6e6cd66aedebdcab24976e0c4d
pranav <sripranav.y19@iiits.in>
2023-03-16 18:28:36 +0530

    vowels

--- src/main/scala/YintiSriPranav/vowels.scala
+++ src/main/scala/YintiSriPranav/vowels.scala
@@ -1,11 +1,15 @@
 package YintiSriPranav
 
-object vowels extends App {
-  def countVowels(words: List[String]): List[Int] = {
-    val vowels = Set('a', 'e', 'i', 'o', 'u')
-    words.map(_.count(vowels))
+object vowels extends App{
+  def countVowels(str: String): Int = {
+    str.count("AEIOUaeiou".contains(_))
   }
-  val words = List("My", "name", "is", "Yinti", "Sri", "Pranav" )
+
+  def countVowels(words: List[String]): List[Int] = {
+    words.map(countVowels)
+  }
+
+  val words = List("My", "name", "is", "Yinti", "Sri", "Pranav")
   val vowelCounts = countVowels(words)
   println(vowelCounts)
 }
