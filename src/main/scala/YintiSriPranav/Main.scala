786e188354fef5da6e9d1bbdc515d875cde2ed3a
pranav <sripranav.y19@iiits.in>
2023-03-21 12:48:06 +0530

    VowelsInWord

--- src/main/scala/YintiSriPranav/Main.scala
+++ src/main/scala/YintiSriPranav/Main.scala
@@ -2,12 +2,13 @@ package YintiSriPranav
 import YintiSriPranav.factorial.factorial
 import YintiSriPranav.Fibonacci.fibonacci
 import YintiSriPranav.lastelement.last
-import YintiSriPranav.assign4.VowelsInAWord
+import YintiSriPranav.VowelsInWord.vowelsInAWord
 object Main extends App {
   //println("Welcome to Scala")
   assert(factorial(5,1)==120,"wrong result")
   assert(fibonacci (10)==34, "wrong result")
   assert(last(List(1,2,3,4,5,6))==6)
   val list1= List("Yinti", "Sri", "Pranav")
-  list1.map(x => println(VowelsInAWord(x)))
+  list1.map(x => println(vowelsInAWord(x)))
+
 }
