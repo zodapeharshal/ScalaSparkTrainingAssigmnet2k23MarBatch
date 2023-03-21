1e07920ec3d28f5b7a1a4e6ec7b484e8bf4a43ed
Harsshath23 <harsshath0819@gmail.com>
2023-03-21 15:26:54 +0530

    Modified Main.scala

--- src/main/scala/HarsshathMatcha/Main.scala
+++ src/main/scala/HarsshathMatcha/Main.scala
@@ -2,8 +2,9 @@ package HarsshathMatcha
 
 import HarsshathMatcha.Factorial.factorial
 import HarsshathMatcha.Fibonacci.fibonacci
+import HarsshathMatcha.FindNthHighestElement.findNthHighestElement
 import HarsshathMatcha.LastElement.last
-import HarsshathMatcha.findNthHighest.findNthHighest
+
 
 
 
@@ -11,6 +12,6 @@ object Main extends App {
   assert(factorial(5)==120,"wrong ans")
   assert(fibonacci(9)==34, "wrong ans")
   assert(last(List(1,2,3,4,5,6))==6,"Wrong ans")
-  assert(findNthHighest(List(4,3,5,1,7,6,9,10),3)==7,"Wrong Ans")
+  assert(findNthHighestElement(List(4,3,5,1,7,6,9,10),3)==7,"Wrong Ans")
 
 }
