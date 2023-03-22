fb46637d6a80a91b405f57174e3b2c47c8118f18
shlokthakkar <58928351+shlokthakkar@users.noreply.github.com>
2023-03-23 01:51:47 +0530

    Completed Q3 to Q11

--- src/main/scala/ShlokThakkar/Assignments.scala
+++ src/main/scala/ShlokThakkar/Assignments.scala
@@ -1,28 +1,57 @@
 package ShlokThakkar
 
 import scala.collection.mutable.ListBuffer
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 class Assignments{
 
-  //Pack consecutive duplicates of list elements into sublist.
+
+  //Nth highest element from a list
+  def nthHighest(n:Int, inputList: List[Int]): Int =
+  {
+    if(n==1)
+      {
+        inputList.max
+      }
+    else {
+      nthHighest(n-1,dropElements(inputList.indexOf(inputList.max),inputList))
+    }
+  }
+
+
+  //Sort words in a list based on length.
+  def sortByLength(inputList: List[String]): List[String] = {
+    inputList.sortBy(x => x.length)
+  }
 
 
   //check is sum of all the element in the list is divisible by X or not.
   def divisibleByX(x: Int, inputList: List[Int]): Boolean = {
-    if (x == 0) {
-      println("X can not be 0")
-      false
+    Try{
+      inputList.sum%x
     }
-    else {
-      if (inputList.sum % x == 0) {
-        true
+    match{
+      case Success(_) => {
+        if (x == 0) {
+        println("X can not be 0")
+        false
+        }
+        else {
+          if (inputList.sum % x == 0) {
+          true
+          }
+          else {
+            false
+          }
+        }
       }
-      else {
+      case Failure(_) => {
+        throw new Exception("X cannot be zero")
         false
       }
     }
   }
+
 
   //Insert element at given position
   def insertElement(element:Int,position: Int,inputList :List[Int],outputList :List[Int] = List()): List[Any] =
@@ -34,6 +63,7 @@ class Assignments{
       insertElement(element,position-1,inputList.tail, outputList++List(inputList.head))
     }
   }
+
 
   //Split a list into two parts.
   def splitList(position :Int,inputList:List[Int],subList1:List[Int]=List()):List[List[Int]] =
@@ -48,11 +78,31 @@ class Assignments{
   }
 
 
+  //Drop every Nth element from list
+  def dropElements(n:Int, inputList:List[Int]): List[Int] =
+  {
+    inputList.take(n)++inputList.drop(n+1)
+  }
+
+
+  //Duplicate Elements given number of times
+  def duplicateElements(n:Int,inputList:List[Char],outputList:List[Char] = List() ): List[Char] =
+  {
+    if(inputList.tail.length==0) {
+      outputList++List.fill(n)(inputList.head)
+    }
+    else{
+      duplicateElements(n, inputList.tail, outputList++List.fill(n)(inputList.head))
+    }
+  }
+
 
   //Vowels in a list of stringss
-//  def vowelInWords(input: List[String], output: List[Int] = List()): List[Int] = {
-//    val VOWELS = "aeiouAEIOU"
-//    input.map()
-//  }
+  def vowelInWords(input: List[String], output: List[Int] = List()): List[Int] = {
+    val VOWELS = "aeiouAEIOU"
+    input.map(x=>{
+      x.count(VOWELS.contains(_))
+    })
+  }
 
 }
--- src/main/scala/ShlokThakkar/CountSquares.scala
+++ src/main/scala/ShlokThakkar/CountSquares.scala
@@ -1,11 +1,9 @@
 package ShlokThakkar
 
-object Assignment1{
-
+class CountSquares {
   def countSquares(array: Array[Int]): Int =
   {
     val count =  array.count(x=> Math.sqrt(x)%1==0)
     count
   }
 }
-
