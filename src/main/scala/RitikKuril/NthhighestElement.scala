9f9bf61f3304f18044ef6feb3b1e663a3d50e69d
ritikkuril <ritikkuril@users.noreply.github.com>
2023-03-24 03:29:54 +0530

    changed comments

--- src/main/scala/RitikKuril/NthhighestElement.scala
+++ src/main/scala/RitikKuril/NthhighestElement.scala
@@ -2,11 +2,11 @@ package RitikKuril
 // nth highest Element
 object NthhighestElement {
 
-/*  def nthhighestelement(list:List[Int],n:Int):Int={
+  def nthhighestelement(list:List[Int],n:Int):Int={
 
     list.sorted.reverse(n-1)
   }
-*/
+
 
 
   def nthHighest(list: List[Int], n: Int): Int = {
