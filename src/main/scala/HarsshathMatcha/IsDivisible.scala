41a537bdd1448abe3e23712dabb35bb7abb37a38
Harsshath23 <harsshath0819@gmail.com>
2023-03-25 12:06:38 +0530

    Is divisible by x

---
+++ src/main/scala/HarsshathMatcha/IsDivisible.scala
@@ -0,0 +1,17 @@
+package HarsshathMatcha
+import scala.annotation.tailrec
+object IsDivisible  {
+
+  def isDivisibleElements(list:List[Int],x:Int):Boolean={
+    @tailrec
+    def isDiv(list:List[Int],x:Int,sum:Int):Boolean={
+      if(list.isEmpty) sum%x == 0
+      else isDiv(list.tail,x,sum=sum+list.head)
+    }
+    isDiv(list,x,0)
+  }
+  //al list = List(1,6,8,4,5,9,2)
+  //val x= 7
+  //println(isDivisibleElements(list,x))
+
+}
